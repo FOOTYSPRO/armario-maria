359f44a0203c904059e0d03e28507932f7d9cd5e
FOOTYSPRO <footys1024@gmail.com>
2026-01-17 13:39:08 +0100

    Arreglando version Nextjs 🔧

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Esto obliga a Next.js a procesar las librerías modernas de Firebase
+    // Esto ayuda a que Vercel procese bien las librerías de Firebase
     transpilePackages: ['undici', 'firebase', '@firebase/storage', '@firebase/firestore', '@firebase/auth'],
 };
 
